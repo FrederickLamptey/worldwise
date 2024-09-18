38e196db5617ca5506d66929548a55d0a59d11ac
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-09-17 20:38:51 -0400

    create the AppLayout component and set up a fake api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product"
-import Pricing from "./pages/Pricing"
-import Homepage from "./pages/HomePage";
-import AppLayout from "./pages/AppLayout"
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/HomePage';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+import CityList from './components/CityList';
 
 function App() {
   return (
@@ -13,7 +14,12 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
