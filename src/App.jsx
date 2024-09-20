1b1f69bdf3dc89563130e795dd1abc560e3cf1ca
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-09-19 20:15:25 -0400

    store the id, lat and lng state in URL

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList';
+import City from './components/City';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -41,11 +43,15 @@ function App() {
             index
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />}></Route>
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="login" element={<Login />} />
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -3,6 +3,7 @@
 // }
 
 // export default CityItem;
+import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css'
 
 const formatDate = (date) =>
@@ -15,16 +16,18 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
   
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id, position } = city;
 
-  return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+  return ( 
+    <li>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
-  )
+  );
 }
 
 export default CityItem
