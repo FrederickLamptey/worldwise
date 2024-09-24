deb500382180ce63b9e7881f4fbb84264e7ae75a
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-09-24 18:53:10 -0400

    create City view and BackButton component.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,10 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from "react";
-
+import BackButton from "./BackButton";
 import styles from "./Form.module.css";
 import Button from "./Button";
-import { useNavigate } from "react-router-dom";
+
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -15,7 +15,7 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate()
+ 
 
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
@@ -55,13 +55,7 @@ function Form() {
       <div className={styles.buttons}>
         {/* <button>Add</button> */}
         <Button type='primary'>Add</Button>
-        <Button
-          type='back'
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}>&larr; Back</Button>
-        {/* <button>&larr; Back</button> */}
+        <BackButton />
       </div>
     </form>
   );
