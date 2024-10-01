4871fd7d6d07bf806637c2be4968c78d9a357826
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-10-01 16:15:41 -0400

    write a conditional state to check the id in getCity

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -75,6 +75,8 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) { 
+    if (Number(id) === currentCity.id) return;
+    
     dispatch({ type: 'loading' });
     try {
       // setIsLoading(true);
